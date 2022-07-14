87bcc50188fda05434d4a1a8080e7563e49131ef
Vikash Shaw <vikash.shaw@imerit.net>
2022-07-14 17:00:55 +0530

    fix: region in auth handler

--- api/authenticate-google.js
+++ api/authenticate-google.js
@@ -3,7 +3,7 @@
  */
 
  const AWS = require('aws-sdk');
- AWS.config.update({ region: 'us-west-2' });
+ AWS.config.update({ region: 'us-east-1' });
  
  const jwtDecode = require('jwt-decode');
  const util = require('./util.js');
